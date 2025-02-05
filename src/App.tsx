2e3f3344c324ead085888dd742a89d635edaf9a9
Mitchell Wallace <38238516+mitchell-wallace@users.noreply.github.com>
2025-02-05 14:16:10 +1100

    Basic layout using MantineUI as a base

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import { HeaderSimple } from './components/.MantineUI/HeaderSimple/HeaderSimple';
+import { FooterCentered } from './components/.MantineUI/FooterCentered/FooterCentered';
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
+      <HeaderSimple />
       <Router />
+      <FooterCentered />
     </MantineProvider>
   );
 }
